ebf929e4d09d1eb6e79349517d513ede1df8b8a8
mekya <ahmetmermerkaya@gmail.com>
2013-06-30 17:22:56 +0300

    Redundant files, related to HTTP server, are removed

--- StreamServerJava/src/com/mekya/streamserver/Main.java
+++ StreamServerJava/src/com/mekya/streamserver/Main.java
@@ -1,10 +1,6 @@
 package com.mekya.streamserver;
 
-import java.io.IOException;
-
-import com.mekya.streamserver.servers.HttpServer;
 import com.mekya.streamserver.servers.RtspServer;
-import com.mekya.streamserver.servers.StreamTcpServer;
 import com.mekya.streamserver.servers.StreamUdpServer;
 
 public class Main {
@@ -13,19 +9,6 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-//		StreamTcpServer messenger = new StreamTcpServer(53007);
-//		
-//		HttpServer server = new HttpServer(24007);
-//		server.setVideoStreamer(messenger);
-//		server.setAudioStreamer(messenger);
-//		
-//		try {
-//			messenger.start();
-//			server.start();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-		
 		StreamUdpServer udpServer = new StreamUdpServer();
 		RtspServer rtspServer = new RtspServer(udpServer);
 		
