950023b29fa5f9e582fe678cd98b5bf10995ceed
murat <murat@murat1>
2013-08-18 15:15:46 +0300

    vitamio projeye gömüldüğü için repo'dan eski ve gerekmeyen projeler silindi.

--- StreamSender/src/com/mekya/live/streamsender/IStreamer.java
+++
@@ -1,9 +0,0 @@
-package com.mekya.live.streamsender;
-
-public interface IStreamer {
-
-	public void startAudio(String address, int port);
-	public void startVideo(String address, int port);
-
-	public void stopStreaming();
-}
