34ae25d97fba134643d5d6e002a3cd50dd1ead8f
mekya <ahmetmermerkaya@gmail.com>
2013-09-08 14:44:12 +0300

    Refactor: Right bar in ClientActivity(that plays video) is removed.

--- Butterfly_JavaCV/src/com/butterfly/ClientActivity.java
+++ Butterfly_JavaCV/src/com/butterfly/ClientActivity.java
@@ -26,10 +26,6 @@ public class ClientActivity extends Activity {
 	//	private SurfaceView surfaceView;
 	private VideoView videoView;
 
-	private ToggleButton startButton;
-
-	private EditText serverAddressEditText;
-
 	protected Process ffmpegAudioProcess;
 
 	@Override
@@ -47,7 +43,6 @@ public class ClientActivity extends Activity {
 		setContentView(R.layout.activity_client);
 		
 
-		serverAddressEditText = (EditText) findViewById(R.id.portNumText);
 
 		videoView = (VideoView) findViewById(R.id.surface_view);
 		
@@ -100,74 +95,7 @@ public class ClientActivity extends Activity {
 				}
 			});
 		}
-		
 
-		startButton = (ToggleButton) findViewById(R.id.button_start);
-
-		startButton.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View arg0) {
-
-				if (startButton.isChecked()) {
-					
-					//videoView.setVideoPath("http://"+serverAddressEditText.getText().toString()+":24007/liveVideo");
-					//videoView.setVideoPath("rtsp://"+serverAddressEditText.getText().toString()+":6454/live.ts");
-					videoView.setVideoPath(Environment.getExternalStorageDirectory()+"/Video/small.mp4");
-					
-					videoView.setVideoQuality(MediaPlayer.VIDEOQUALITY_HIGH);
-					videoView.setBufferSize(1024*10);
-
-
-
-					videoView.setOnBufferingUpdateListener(new OnBufferingUpdateListener() {
-						int i = 0;
-						@Override
-						public void onBufferingUpdate(MediaPlayer arg0, int percent) {
-
-							if (percent>=0 && videoView.isPlaying() == false) {
-								videoView.start();
-							}
-							if (i > 20) {
-								Log.i("Buffer update", "buffer " + percent);
-								i = 0;
-							}
-							i++;
-
-						}
-					});
-
-					videoView.setOnInfoListener(new OnInfoListener() {
-
-						@Override
-						public boolean onInfo(MediaPlayer arg0, int what, int extra) {
-							if (what == MediaPlayer.MEDIA_INFO_BUFFERING_START) {
-								videoView.start();
-							}
-							else if (what == MediaPlayer.MEDIA_INFO_BUFFERING_END && videoView.isPlaying() == false) {
-								videoView.start();
-							}
-							return false;
-						}
-					});
-
-
-					videoView.setOnErrorListener(new OnErrorListener() {
-
-						@Override
-						public boolean onError(MediaPlayer arg0, int arg1, int arg2) {
-							System.out.println("Error on media player arg1:" + arg1 + " arg2:"+ arg2);
-							return false;
-						}
-					});
-				}
-				else {
-					stopStreaming();
-				}
-
-
-			}
-		});
 	}
 
 	
