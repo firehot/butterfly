4dc7a6c1c469dc9b571423dcee9a627eb89201b4
murats <salman.murat@gmail.com>
2013-12-22 01:32:54 +0200

    Manifesto dosyasındaki StreamList activity si çıkarıldı. Artık fragment olduğu için buna ihtiyaç yok. Diğer dosyalarda refactor yapıldı

--- Butterfly_JavaCV/src/com/butterfly/MainActivity.java
+++ Butterfly_JavaCV/src/com/butterfly/MainActivity.java
@@ -1,49 +1,30 @@
 package com.butterfly;
 
-import java.util.zip.Inflater;
-
-import com.bugsense.trace.BugSenseHandler;
-import com.butterfly.debug.BugSense;
-import com.butterfly.fragment.MapFragment;
-import com.butterfly.fragment.StreamListFragment;
-import com.butterfly.fragment.StreamListFragment.GetStreamListTask;
-import com.googlecode.javacv.cpp.opencv_ml.CvSVMSolver.GetRow;
-
 import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.app.FragmentTransaction;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.ListFragment;
 import android.support.v4.view.ViewPager;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.Window;
 
+import com.bugsense.trace.BugSenseHandler;
+import com.butterfly.debug.BugSense;
+import com.butterfly.fragment.MapFragment;
+import com.butterfly.fragment.StreamListFragment;
+
 public class MainActivity extends FragmentActivity implements ActionBar.TabListener {
 
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide fragments for each of the
-	 * three primary sections of the app. We use a {@link android.support.v4.app.FragmentPagerAdapter}
-	 * derivative, which will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a {@link android.support.v4.app.FragmentStatePagerAdapter}.
-	 */
 	AppSectionsPagerAdapter mAppSectionsPagerAdapter;
-
-	/**
-	 * The {@link ViewPager} that will display the three primary sections of the app, one at a
-	 * time.
-	 */
 	ViewPager mViewPager;
-
 	private static StreamListFragment streamListFragment;
 	private static MapFragment mapFragment;
 
@@ -87,18 +68,12 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 		mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
 			@Override
 			public void onPageSelected(int position) {
-				// When swiping between different app sections, select the corresponding tab.
-				// We can also use ActionBar.Tab#select() to do this if we have a reference to the
-				// Tab.
 				actionBar.setSelectedNavigationItem(position);
 			}
 		});
 
 		// For each of the sections in the app, add a tab to the action bar.
 		for (int i = 0; i < mAppSectionsPagerAdapter.getCount(); i++) {
-			// Create a tab with text corresponding to the page title defined by the adapter.
-			// Also specify this Activity object, which implements the TabListener interface, as the
-			// listener for when this tab is selected.
 			actionBar.addTab(
 					actionBar.newTab()
 					.setText(mAppSectionsPagerAdapter.getPageTitle(i))
@@ -108,7 +83,6 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// TODO Auto-generated method stub
 		getMenuInflater().inflate(R.menu.activity_client, menu);
 		return true;
 	}
@@ -133,7 +107,6 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
 	@Override
 	public void onDestroy() {
-		// TODO Auto-generated method stub
 		super.onDestroy();
 
 		BugSenseHandler.closeSession(this);
