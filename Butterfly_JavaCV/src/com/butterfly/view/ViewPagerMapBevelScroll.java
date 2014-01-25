12425af32c27c7d842a25625ee38eeb94799befa
murat <maydin@gmail.com>
2014-01-25 18:41:53 +0200

    resolves #130 Map fragment indexini guncellemek unutulabiliyor, index bakmak yerine tipe bakarak yapildi.

--- Butterfly_JavaCV/src/com/butterfly/view/ViewPagerMapBevelScroll.java
+++ Butterfly_JavaCV/src/com/butterfly/view/ViewPagerMapBevelScroll.java
@@ -1,14 +1,17 @@
 package com.butterfly.view;
 
 import android.content.Context;
+import android.support.v4.app.Fragment;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.butterfly.adapter.AppSectionsPagerAdapter;
+import com.butterfly.fragment.MapFragment;
 
 public class ViewPagerMapBevelScroll extends android.support.v4.view.ViewPager {
 
     private static final int DEFAULT_SWIPE_MARGIN_WIDTH_DIP = 100;
     private int swipeMarginWidth;
-    private static final int MAP_FRAGMENT_INDEX = 2;
 
     public ViewPagerMapBevelScroll(Context context) {
         super(context);
@@ -29,9 +32,13 @@ public class ViewPagerMapBevelScroll extends android.support.v4.view.ViewPager {
     protected boolean canScroll(View v, boolean checkV, int dx, int x, int y) {
             if (v instanceof ViewPagerMapBevelScroll) {
             	
-                if (getCurrentItem() == MAP_FRAGMENT_INDEX) {
-                    return !isAllowedMapSwipe(x, dx);
-                }
+            	AppSectionsPagerAdapter adapter = (AppSectionsPagerAdapter)getAdapter();
+            	Fragment fragment = adapter.getItem(getCurrentItem());
+            	
+            	if(fragment instanceof MapFragment)
+            	{
+            		return !isAllowedMapSwipe(x, dx);
+            	}
             }
             return super.canScroll(v, checkV, dx, x, y);
         }
