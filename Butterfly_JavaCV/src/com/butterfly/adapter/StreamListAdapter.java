469f32729bd4f982fe027396e58e16cd7acf9651
murat <maydin@gmail.com>
2014-03-23 15:29:52 +0200

    Stream Item için overflow button koyuldu.

--- Butterfly_JavaCV/src/com/butterfly/adapter/StreamListAdapter.java
+++ Butterfly_JavaCV/src/com/butterfly/adapter/StreamListAdapter.java
@@ -7,32 +7,36 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.butterfly.R;
+import com.butterfly.fragment.StreamListFragment;
 import com.butterfly.fragment.StreamListFragment.Stream;
 import com.loopj.android.image.SmartImageView;
 
 public class StreamListAdapter extends ArrayAdapter<Stream> {
-	private final Activity context;
+
+	private final StreamListFragment fragment;
 
 	static class ViewHolder {
 		public TextView textStreamName;
 		public TextView textStreamViewerCount;
 		public SmartImageView imageView;
 		public TextView liveNowView;
+		public Button overflowButton;
 	}
 
-	public StreamListAdapter(Activity context) {
-		super(context, R.layout.stream_list_item);
-		this.context = context;
+	public StreamListAdapter(StreamListFragment fragment) {
+		super(fragment.getActivity(), R.layout.stream_list_item);
+		this.fragment = fragment;		
 	}
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		View rowView = convertView;
 		if (rowView == null) {
-			LayoutInflater inflater = context.getLayoutInflater();
+			LayoutInflater inflater = this.fragment.getActivity().getLayoutInflater();
 			rowView = inflater.inflate(R.layout.stream_list_item, null);
 			ViewHolder viewHolder = new ViewHolder();
 			viewHolder.textStreamName = (TextView) rowView
@@ -42,6 +46,8 @@ public class StreamListAdapter extends ArrayAdapter<Stream> {
 			viewHolder.imageView = (SmartImageView) rowView
 					.findViewById(R.id.stream_image);
 			viewHolder.liveNowView = (TextView) rowView.findViewById(R.id.live_now);
+			viewHolder.overflowButton = (Button) rowView.findViewById(R.id.streamOverflow);
+			viewHolder.overflowButton.setOnClickListener(this.fragment);
 			rowView.setTag(viewHolder);
 		}
 
@@ -53,14 +59,14 @@ public class StreamListAdapter extends ArrayAdapter<Stream> {
 			streamname = s.name.substring(0, 17)+"...";
 
 		holder.textStreamName.setText(streamname);
-		String image_url = this.context.getString(R.string.http_url)+s.url+".png";
+		String image_url = this.fragment.getActivity().getString(R.string.http_url)+s.url+".png";
 		holder.imageView.setImageUrl(image_url);
 
 		if(!s.isLive)
 			holder.liveNowView.setText("");
 		
 		if (s.viewerCount > 0) {
-			holder.textStreamViewerCount.setText(context.getResources()
+			holder.textStreamViewerCount.setText(this.fragment.getActivity().getResources()
 					.getQuantityString(R.plurals.viewers_count, s.viewerCount,
 							s.viewerCount));
 			holder.textStreamViewerCount.setVisibility(View.VISIBLE);
